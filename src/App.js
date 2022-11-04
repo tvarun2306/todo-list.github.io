2aa997a829ab2eb1119a46bd68cb73861804644d
tvarun2306 <108822631+tvarun2306@users.noreply.github.com>
2022-11-04 17:25:41 +0530

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React ,{useState} from 'react';
 
 function App() {
+  const [note, setNote] = useState();
+  const [noteList, setNoteList] = useState([]);
+  const [check, setCheck] = useState(false);
+
+  function handleNoteList() {
+    const noteListtemp = [...noteList];
+    noteListtemp.push(note);
+    setNoteList(noteListtemp);
+  }
+
+  function handleDelete(index) {
+    const noteListTemp = [...noteList];
+    noteListTemp.splice(index, 1);
+    setNoteList(noteListTemp);
+  }
+
+  // function handleNote(e) {
+  //   setNote(e.target.value);
+  // }
+
+  function handleCheck() {
+    setCheck(true);
+    if (check === true) {
+      return <button>del</button>;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="heading">To Do List-</h1>
+      <input
+        class="input"
+        type="text"
+        value={note}
+        onChange={(e) => {
+          setNote(e.target.value);
+        }}
+      ></input>
+
+      <button onClick={handleNoteList}>Add Note</button>
+      <div class="noteList">
+        {noteList.map((item, index) => (
+          <div class="listElement">
+            <div>
+              {" "}
+              {index + 1 + ". "}
+              {item}
+            </div>
+            <div>
+              <input
+                class="checkBox"
+                type="checkbox"
+                onChange={handleCheck}
+              ></input>
+              <button id="delete" onClick={handleDelete}>
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
